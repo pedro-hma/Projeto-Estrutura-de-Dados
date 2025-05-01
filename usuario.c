8629d22ae1b9190ac936f1e4a23cb931df99d7bc
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2025-05-01 15:03:05 +0000

    Código - Usuario

---
+++ usuario.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void cadastrarUsuario() { printf("Cadastro de usuário (em desenvolvimento)\n"); }
+void consultarUsuario() { printf("Consulta de usuário (em desenvolvimento)\n"); }
+void carregarUsuarios() {}
+void salvarUsuarios() {}
