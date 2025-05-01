5714b53045162afa113d33830c8809c13f9b3cd8
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2025-05-01 15:31:26 +0000

    mudando usuario.h

--- src/usuario.h
+++ src/usuario.h
@@ -1,7 +1,27 @@
 #ifndef USUARIO_H
 #define USUARIO_H
-void cadastrarUsuario();
-void consultarUsuario();
-void carregarUsuarios();
-void salvarUsuarios();
+
+#define TAM_HASH_USUARIO 100
+
+typedef struct Usuario {
+    int id;
+    char nome[100];
+    struct Usuario* prox;
+} Usuario;
+
+typedef struct {
+    Usuario* tabela[TAM_HASH_USUARIO];
+} UsuarioHash;
+
+// Prototótipos das funções
+UsuarioHash* criarTabelaUsuarios();
+Usuario* criarUsuario(int id, const char* nome);
+void inserirUsuario(UsuarioHash* tabela, Usuario* usuario);
+Usuario* buscarUsuario(UsuarioHash* tabela, int id);
+void salvarUsuarios(UsuarioHash* tabela, const char* nomeArquivo);
+void carregarUsuarios(UsuarioHash* tabela, const char* nomeArquivo);
+void cadastrarUsuario(UsuarioHash* tabela);
+void consultarUsuario(UsuarioHash* tabela);
+void liberarUsuarios(UsuarioHash* tabela);
+
 #endif
