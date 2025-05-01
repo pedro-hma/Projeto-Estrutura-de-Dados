553bbad11fd8a36ec655dd3ba5d1a907e6693e84
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2025-05-01 14:57:01 +0000

    criando o arquivo main

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//#include "livro.h"
+//#include "usuario.h"
+//#include "emprestimo.h"
+
+int main() {
+    int opcao;
+
+    carregarDados(); // Carrega arquivos para memória
+
+    do {
+        printf("\n===== Sistema de Biblioteca =====\n");
+        printf("1. Cadastrar Livro\n");
+        printf("2. Cadastrar Usuário\n");
+        printf("3. Realizar Empréstimo\n");
+        printf("4. Realizar Devolução\n");
+        printf("5. Consultar Livro\n");
+        printf("6. Consultar Usuário\n");
+        printf("0. Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch(opcao) {
+            case 1: cadastrarLivro(); break;
+            case 2: cadastrarUsuario(); break;
+            case 3: realizarEmprestimo(); break;
+            case 4: realizarDevolucao(); break;
+            case 5: consultarLivro(); break;
+            case 6: consultarUsuario(); break;
+        }
+    } while (opcao != 0);
+
+    salvarDados(); // Salva alterações nos arquivos
+    return 0;
+}
